49c4df681b54d38a46f9ea2d9a8c935197f5ba64
Jorge Andrade <profesorjand@gmail.com>
2024-02-24 20:09:22 -0300

    fix appendchild

--- showChampions.js
+++ showChampions.js
@@ -1,9 +1,13 @@
 async function showChampions(){
+    const championsContainer = document.getElementById("championsContainer");
+    console.log(championsContainer);
+    console.log("hola")
     const version = "14.3"
     const urlDdragon= `https://raw.communitydragon.org/${version}/game/`;
-    const url="https://backend-profesorjand.onrender.com/tft/champions"
-    const champions = await fetch(url).then(resp=>resp.json()).then(data=>data);
-    const championsContainer = document.getElementById("championsContainer");
+    const url="https://backend-profesorjand.onrender.com/tft/champions";
+    try{
+        const champions = await fetch(url).then(resp=>resp.json()).then(data=>data).catch(err=>console.log(err));
+    }catch(err){console.log(err)};
     console.log(champions)
     champions.forEach(element => {
         const imagenElem = document.createElement("img");
