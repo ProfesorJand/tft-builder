afe71c57da4c8a6f5a4175448b7561c00d5acd84
Jorge Andrade <profesorjand@gmail.com>
2024-02-24 19:10:26 -0300

    showChampions testing1

--- showChampions.js
+++ showChampions.js
@@ -1,8 +1,16 @@
 async function showChampions(){
     const url="https://backend-profesorjand.onrender.com/tft/champions"
     const champions = await fetch(url).then(resp=>resp.json()).then(data=>data);
-    //const championsContainer = window.document.getElementById("championsContainer");
+    const championsContainer = window.document.getElementById("championsContainer");
     console.log(champions)
+    const divElem = document.createElement("div")
+    const imagenElem = document.createElement("img")
+    champions.forEach(element => {
+        const src = element.tileIcon.replace(".dds",".png").toLowerCase();
+        imagenElem.src=src;
+        divElem.appendChild(imagenElem);
+        championsContainer.appendChild(divElem);
+    });
     //console.alert(championsContainer)
 }
 
