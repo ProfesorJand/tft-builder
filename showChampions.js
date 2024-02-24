c4317275e2bc1ad10ab36c75e5083281e8796b98
Jorge Andrade <profesorjand@gmail.com>
2024-02-24 19:52:39 -0300

    championsContainer append

--- showChampions.js
+++ showChampions.js
@@ -3,15 +3,14 @@ async function showChampions(){
     const urlDdragon= `https://raw.communitydragon.org/${version}/game/`;
     const url="https://backend-profesorjand.onrender.com/tft/champions"
     const champions = await fetch(url).then(resp=>resp.json()).then(data=>data);
-    const championsContainer = window.document.getElementById("championsContainer");
+    const championsContainer = document.getElementById("championsContainer");
     console.log(champions)
-    const divElem = document.createElement("div")
-    const imagenElem = document.createElement("img")
+    const imagenElem = document.createElement("img");
     champions.forEach(element => {
         const src = urlDdragon + element.tileIcon.replace(".dds",".png").toLowerCase();
         imagenElem.src=src;
-        divElem.appendChild(imagenElem);
-        championsContainer.appendChild(divElem);
+        imagenElem.alt="champion "+ element.
+        championsContainer.appendChild(imagenElem);
     });
     //console.alert(championsContainer)
 }
