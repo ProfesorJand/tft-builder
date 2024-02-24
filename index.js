c958a78843235f4ae02a2f04d180917488fe9f68
Jorge Andrade <profesorjand@gmail.com>
2024-02-24 20:59:15 -0300

    drag and drop need fix in adding only one

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ function allowDrop(ev){
 
 function drop(ev) {
     ev.preventDefault();
-    var data=ev.dataTransfer.getData("text/html");
+    var data=ev.dataTransfer.getData("text");
+    console.log("data",data);
+    console.log("document",document.getElementById(data))
+
     /* Within a Mouse event you can even check the status of some Keyboard-Buttons
        such as CTRL, ALT, SHIFT. */
     if (ev.ctrlKey)
@@ -16,3 +19,9 @@ function drop(ev) {
     else
       ev.target.appendChild(document.getElementById(data));
   }
+
+  function drag(ev) {
+    console.log(ev.target.id)
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+
--- showChampions.js
+++ showChampions.js
@@ -2,21 +2,28 @@ async function showChampions(){
     const championsContainer = document.getElementById("championsContainer");
     console.log(championsContainer);
     console.log("hola")
-    const version = "14.3"
+    const version = "14.3";
     const urlDdragon= `https://raw.communitydragon.org/${version}/game/`;
     const url="https://backend-profesorjand.onrender.com/tft/champions";
     try{
-        const champions = await fetch(url).then(resp=>resp.json()).then(data=>data).catch(err=>console.log(err));
+        const champions = await fetch(url).then(resp=>resp.json()).then(data=>{
+            data.forEach(element => {
+                const imagenElem = document.createElement("img");
+                const src = urlDdragon + element.tileIcon.replace(".dds",".png").replace("tex","png").toLowerCase();
+                imagenElem.src=src;
+                imagenElem.alt="champion "+ element.name;
+                imagenElem.className = "img-champions";
+                imagenElem.id = "champion-"+ element.name.replace(" ","-");
+                imagenElem.draggable=true 
+                imagenElem.addEventListener("dragstart",(ev)=>{
+                    console.log(ev.target.id)
+                    ev.dataTransfer.setData("text", ev.target.id);
+                });
+                championsContainer.appendChild(imagenElem);
+            });
+        console.log(champions);
+        }).catch(err=>console.log(err));
     }catch(err){console.log(err)};
-    console.log(champions)
-    champions.forEach(element => {
-        const imagenElem = document.createElement("img");
-        const src = urlDdragon + element.tileIcon.replace(".dds",".png").toLowerCase();
-        imagenElem.src=src;
-        imagenElem.alt="champion "+ element.
-        championsContainer.appendChild(imagenElem);
-    });
+    
     //console.alert(championsContainer)
 }
-
-showChampions();
