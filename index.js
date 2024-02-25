99f6e4eccc5a27f66eb47d24ad7203f66e2e0bb9
Jorge Andrade <profesorjand@gmail.com>
2024-02-25 16:51:22 -0300

    change hex css and fixing champ img

--- index.js
+++ index.js
@@ -7,17 +7,28 @@ function drop(ev) {
     var data=ev.dataTransfer.getData("text");
     console.log("data",data);
     console.log("document",document.getElementById(data))
+    const championNode = document.getElementById(data);
+    const targetNode = ev.target;
+    console.log("championNode.dataset.fromBoard",championNode.dataset.fromBoard)
 
     /* Within a Mouse event you can even check the status of some Keyboard-Buttons
        such as CTRL, ALT, SHIFT. */
-    if (ev.ctrlKey)
+    if (championNode.dataset.fromBoard === "true")
     {
-      var nodeCopy = document.getElementById(data).cloneNode(true);
-      nodeCopy.id = "newId"; /* We cannot use the same ID */
+      var nodeCopy = championNode.cloneNode(true);
+      nodeCopy.id = targetNode.id + "-" + data; /* We cannot use the same ID */;
+      nodeCopy.dataset.fromBoard = false;
+      nodeCopy.addEventListener("dragstart",(ev)=>{
+        console.log(ev.target.id)
+        ev.dataTransfer.setData("text", ev.target.id);
+    });
+
+
       ev.target.appendChild(nodeCopy);
     }
-    else
+    else{
       ev.target.appendChild(document.getElementById(data));
+    }
   }
 
   function drag(ev) {
--- showChampions.js
+++ showChampions.js
@@ -8,22 +8,33 @@ async function showChampions(){
     try{
         const champions = await fetch(url).then(resp=>resp.json()).then(data=>{
             data.forEach(element => {
-                const imagenElem = document.createElement("img");
-                const src = urlDdragon + element.tileIcon.replace(".dds",".png").replace("tex","png").toLowerCase();
-                imagenElem.src=src;
-                imagenElem.alt="champion "+ element.name;
-                imagenElem.className = "img-champions";
-                imagenElem.id = "champion-"+ element.name.replace(" ","-");
-                imagenElem.draggable=true 
-                imagenElem.addEventListener("dragstart",(ev)=>{
-                    console.log(ev.target.id)
-                    ev.dataTransfer.setData("text", ev.target.id);
-                });
-                championsContainer.appendChild(imagenElem);
+                const {name, tileIcon, traits, apiName, cost, ability, squareIcon} = element;
+                if(cost >= 1 && cost <= 5){
+                    
+                    const imagenElem = document.createElement("img");
+                    const src = urlDdragon + (squareIcon.replace("Splashes/","Splashes/patching/").replace("_Mobile.","_mobile_small.") || tileIcon).toLowerCase().replace(".dds",".png").replace("tex","png");
+                    imagenElem.src=src;
+                    imagenElem.alt="champion "+ name;
+                    imagenElem.className = "img-champions";
+                    imagenElem.id = apiName;
+                    imagenElem.dataset.traits = traits;
+                    imagenElem.dataset.cost = cost;
+                    imagenElem.dataset.ability = ability;
+                    imagenElem.dataset.fromBoard = true;
+                    imagenElem.draggable=true;
+                    
+                    imagenElem.addEventListener("dragstart",(ev)=>{
+                        console.log(ev.target.id)
+                        ev.dataTransfer.setData("text", ev.target.id);
+                    });
+                    championsContainer.appendChild(imagenElem);
+                }
             });
-        console.log(champions);
+            console.log(data);
         }).catch(err=>console.log(err));
     }catch(err){console.log(err)};
     
     //console.alert(championsContainer)
 }
+
+showChampions()
